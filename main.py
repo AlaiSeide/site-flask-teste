dadcbcc2981f0509e1dff7458a6957c996080a59
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2024-04-22 22:49:00 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# arquivo que vai rodar o meu site
+
+from meu_site import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ meu_site/__init__.py
@@ -0,0 +1,11 @@
+# As configuracoes do meu site
+from flask import Flask
+
+# criando o meu site
+
+app = Flask(__name__)
+
+
+
+# importandos as rotas do meu site
+from meu_site import routes
